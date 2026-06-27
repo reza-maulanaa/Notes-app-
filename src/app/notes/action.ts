2c9b1034ed5c27f271851ad907cc6f699b80d603
Reza <rezzreborn@gmail.com>
2026-06-27 14:21:52 +0700

    add zod in action.ts (createNote and updateNote) to increase the security

--- src/app/notes/action.ts
+++ src/app/notes/action.ts
@@ -5,9 +5,23 @@ import { notes } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 import { eq, and } from "drizzle-orm";
 import { redirect } from "next/navigation";
-import { auth, signIn } from "@/auth";
+import { auth } from "@/auth";
+import { z } from "zod";
 
+const noteSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Judul terlalu sedikit")
+    .max(50, "Judul terlalu panjang"),
+  content: z
+    .string()
+    .min(1, "Isi terlalu sedikit")
+    .max(300, "Isi terlalu banyak"),
+});
 
+const updateNoteSchema = noteSchema.extend({
+  id: z.number().positive(),
+});
 
 export async function createNote(formData: FormData) {
   const session = await auth();
@@ -19,14 +33,22 @@ export async function createNote(formData: FormData) {
     .where(eq(notes.userId, Number(session.user.id)));
 
   if (session.user.role === "user" && userNotes.length >= 5) {
-    redirect("/notes?error=limit_reached_basic")
+    redirect("/notes?error=limit_reached_basic");
   }
   if (session.user.role === "premium" && userNotes.length >= 15) {
-  redirect("/notes?error=limit_reached_premium");
-}
+    redirect("/notes?error=limit_reached_premium");
+  }
 
-  const title = formData.get("title") as string;
-  const content = formData.get("content") as string;
+  const result = noteSchema.safeParse({
+    title: formData.get("title") as string,
+    content: formData.get("content") as string,
+  });
+
+  if (!result.success) {
+    redirect("/notes?error=invalid_input");
+  }
+
+  const { title, content } = result.data;
 
   await db.insert(notes).values({
     title,
@@ -49,16 +71,24 @@ export async function deleteNote(formData: FormData) {
     .where(and(eq(notes.id, id), eq(notes.userId, Number(session.user.id))));
 
   revalidatePath("/notes");
-  redirect("/notes")
+  redirect("/notes");
 }
 
 export async function updateNote(formData: FormData) {
   const session = await auth();
   if (!session?.user?.id) redirect("/login");
 
-  const id = Number(formData.get("id"));
-  const title = formData.get("title") as string;
-  const content = formData.get("content") as string;
+  const result = updateNoteSchema.safeParse({
+    id: Number(formData.get("id")),
+    title: formData.get("title") as string,
+    content: formData.get("content") as string,
+  });
+
+  if (!result.success) {
+    redirect("/notes?error=invalid_input");
+  }
+
+  const { id, title, content } = result.data;
 
   await db
     .update(notes)
